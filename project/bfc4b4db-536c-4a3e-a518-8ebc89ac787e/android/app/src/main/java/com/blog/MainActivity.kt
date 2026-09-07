deef2716ea9cfc8087eab3a97d7906108bf94763
Francis261 <francisdominic986532@gmail.com>
2026-09-07 04:27:03 +0100

    apkbuilder: add android/app/src/main/java/com/blog/MainActivity.kt

---
+++ project/bfc4b4db-536c-4a3e-a518-8ebc89ac787e/android/app/src/main/java/com/blog/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.blog
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+    override fun getMainComponentName(): String = "blog"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
